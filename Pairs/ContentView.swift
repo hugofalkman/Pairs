2aec4e8b8387bf9c88c3a76f71bf0f2fe6ed1753
hugofalkman <hugo@falkman.net>
2020-09-06 16:22:39 +0200

    Initial Commit

---
+++ Pairs/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pairs
+//
+//  Created by H Hugo Falkman on 06/09/2020.
+//  Copyright © 2020 H Hugo Falkman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
